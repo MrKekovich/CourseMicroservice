6b74f82618be558a7cfc8065d15ac8f5d6d09e59
MrKekovich <mrkekovich.official@gmail.com>
2023-11-04 15:54:36 +0300

    Initial commit

---
+++ src/test/kotlin/com/mrkekovich/courses/CoursesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mrkekovich.courses
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoursesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
