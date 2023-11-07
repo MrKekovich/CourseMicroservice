7a6536aacd4f145402bc20776e9f0f441e7cdb6e
MrKekovich <mrkekovich.official@gmail.com>
2023-11-07 17:14:03 +0300

    feat: Added documentation to CourseDto and function "toEntity" with id

--- src/main/kotlin/com/mrkekovich/courses/dto/CourseDto.kt
+++ src/main/kotlin/com/mrkekovich/courses/dto/CourseDto.kt
@@ -5,7 +5,16 @@ import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 import org.hibernate.validator.constraints.Length
 
-
+/**
+ * Course dto
+ *
+ * Sealed class for course DTOs.
+ * Used to easily validate, convert, map and return data.
+ *
+ * @property name Course name
+ * @property description Course description
+ * @property id (optional) Course ID
+ */
 sealed class CourseDto(
 
     @Length(min = 2, max = 50)
@@ -20,6 +29,14 @@ sealed class CourseDto(
 
     open val id: String? = null,
 ) {
+
+    /**
+     * To entity
+     *
+     * Converts DTO to an entity.
+     *
+     * @return CourseEntity with randomly initialized ID.
+     */
     fun toEntity(): CourseEntity {
         return CourseEntity(
             name = name,
@@ -28,6 +45,31 @@ sealed class CourseDto(
         )
     }
 
+    /**
+     * To entity with ID
+     *
+     * Used to create entity with given ID.
+     *
+     * @param id the ID of entity. Leave empty to generate new ID.
+     * @return CourseEntity with given ID.
+     */
+    fun toEntity(id: String?): CourseEntity {
+        return CourseEntity(
+            name = name,
+            description = description,
+            id = id
+        )
+    }
+
+    /**
+     * Request data class.
+     *
+     * Used to represent requests from clients.
+     * Can be initialized with CourseEntity or with name and description.
+     *
+     * @property name Course name
+     * @property description Course description
+     */
     data class Request(
         override val name: String?,
         override val description: String?
@@ -38,6 +80,15 @@ sealed class CourseDto(
         )
     }
 
+    /**
+     * Response data class.
+     *
+     * Used to represent responses from server.
+     *
+     * @property id ID of course.
+     * @property name Name of course.
+     * @property description Description of course.
+     */
     data class Response(
         @NotNull
         @NotBlank
