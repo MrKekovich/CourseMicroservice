3f1d07cf75283a24b655c04b32914d87f9c9aba4
MrKekovich <mrkekovich.official@gmail.com>
2023-11-13 17:24:33 +0300

    refactor: Enhanced BaseService with concrete CRUD methods and simplified CourseService

--- src/main/kotlin/com/mrkekovich/courses/services/BaseService.kt
+++ src/main/kotlin/com/mrkekovich/courses/services/BaseService.kt
@@ -18,20 +18,39 @@ import kotlin.jvm.optionals.getOrNull
 abstract class BaseService<T : EntityInterface<ID>, ID : Any, RS : BaseDto<T, ID>>(
     private val repository: JpaRepository<T, ID>,
 ) {
+    abstract fun toResponse(entity: T): RS
+
     /**
      * Get all records from the database.
      * @return List of records wrapped in [RS] and [ResponseEntity].
      */
-    abstract fun getAll(): ResponseEntity<List<RS>>
+    open fun getAll(): ResponseEntity<List<RS>> {
+        val response = repository.findAll().map {
+            toResponse(it)
+        }
+        return ResponseEntity(
+            response,
+            HttpStatus.OK
+        )
+    }
 
     /**
      * Get record by ID from the database.
      * @param id ID of the record to get.
      * @return Record wrapped in [RS] and [ResponseEntity].
      */
-    abstract fun getById(
+    open fun getById(
         id: ID
-    ): ResponseEntity<RS>
+    ): ResponseEntity<RS> {
+        val entity = repository.findById(id).getOrNull()
+            ?: return ResponseEntity(
+                HttpStatus.NOT_FOUND
+            )
+        return ResponseEntity(
+            toResponse(entity),
+            HttpStatus.OK
+        )
+    }
 
     /**
      * Create new record in the database.
@@ -39,9 +58,15 @@ abstract class BaseService<T : EntityInterface<ID>, ID : Any, RS : BaseDto<T, ID
      * @param dto Request data.
      * @return New record wrapped in [RS] and [ResponseEntity].
      */
-    abstract fun <RQ : BaseDto<T, ID>> create(
+    open fun <RQ : BaseDto<T, ID>> create(
         dto: RQ
-    ): ResponseEntity<RS>
+    ): ResponseEntity<RS> {
+        val entity = repository.save(dto.toEntity())
+        return ResponseEntity(
+            toResponse(entity),
+            HttpStatus.CREATED
+        )
+    }
 
     /**
      * Update existing record in the database.
@@ -50,17 +75,27 @@ abstract class BaseService<T : EntityInterface<ID>, ID : Any, RS : BaseDto<T, ID
      * @param dto Request data.
      * @return Updated record wrapped in [RS] and [ResponseEntity].
      */
-    abstract fun <RQ : BaseDto<T, ID>> update(
+    open fun <RQ : BaseDto<T, ID>> update(
         id: ID,
         dto: RQ
-    ): ResponseEntity<RS>
+    ): ResponseEntity<RS> {
+        repository.findById(id).getOrNull()
+            ?: return ResponseEntity(
+                HttpStatus.NOT_FOUND
+            )
+        val newEntity = repository.save(dto.toEntity(id))
+        return ResponseEntity(
+            toResponse(newEntity),
+            HttpStatus.OK
+        )
+    }
 
     /**
      * Delete existing record in the database.
      * @param id ID of the record to delete.
      * @return [HttpStatus] wrapped in [ResponseEntity].
      */
-    open fun delete(id: ID): ResponseEntity<Unit> {
+    open fun delete(id: ID): ResponseEntity<HttpStatus> {
         val entity = repository.findById(id).getOrNull()
             ?: return ResponseEntity(
                 HttpStatus.NOT_FOUND
--- src/main/kotlin/com/mrkekovich/courses/services/CourseService.kt
+++ src/main/kotlin/com/mrkekovich/courses/services/CourseService.kt
@@ -1,13 +1,9 @@
 package com.mrkekovich.courses.services
 
-import com.mrkekovich.courses.dto.BaseDto
 import com.mrkekovich.courses.dto.CourseDto
 import com.mrkekovich.courses.models.CourseEntity
 import com.mrkekovich.courses.repositories.CourseRepository
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
-import kotlin.jvm.optionals.getOrNull
 
 /**
  * Course service implementation.
@@ -16,51 +12,9 @@ import kotlin.jvm.optionals.getOrNull
 @Service
 class CourseService(
     private val courseRepository: CourseRepository
-): BaseService<CourseEntity, String, CourseDto.Response>(courseRepository) {
-    override fun getAll(): ResponseEntity<List<CourseDto.Response>> {
-        val response = courseRepository.findAll().map {
-            CourseDto.Response(it)
-        }
-        return ResponseEntity(
-            response,
-            HttpStatus.OK
-        )
-    }
-
-    override fun <RQ : BaseDto<CourseEntity, String>> create(
-        dto: RQ
-    ): ResponseEntity<CourseDto.Response> {
-        val course = courseRepository.save(dto.toEntity())
-        return ResponseEntity(
-            CourseDto.Response(course),
-            HttpStatus.CREATED
-        )
-    }
-
-    override fun <RQ : BaseDto<CourseEntity, String>> update(
-        id: String,
-        dto: RQ
-    ): ResponseEntity<CourseDto.Response> {
-        courseRepository.findById(id).getOrNull()
-            ?: return ResponseEntity(
-                HttpStatus.NOT_FOUND
-            )
-
-        val newCourse = courseRepository.save(dto.toEntity(id = id))
-        return ResponseEntity(
-            CourseDto.Response(newCourse),
-            HttpStatus.OK
-        )
-    }
-
-    override fun getById(id: String): ResponseEntity<CourseDto.Response> {
-        val course = courseRepository.findById(id).getOrNull()
-            ?: return ResponseEntity(
-                HttpStatus.NOT_FOUND
-            )
-        return ResponseEntity(
-            CourseDto.Response(course),
-            HttpStatus.OK
-        )
-    }
+): BaseService<CourseEntity, String, CourseDto.Response>(
+    repository = courseRepository,
+) {
+    override fun toResponse(entity: CourseEntity): CourseDto.Response =
+        CourseDto.Response(entity)
 }
