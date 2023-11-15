d97a8e0fbdc89e56f642edbc34e3c3c9641487a1
MrKekovich <mrkekovich.official@gmail.com>
2023-11-15 08:32:13 +0300

    feat: Added upload request to handle files

--- src/main/kotlin/com/mrkekovich/courses/dto/PhotoDto.kt
+++ src/main/kotlin/com/mrkekovich/courses/dto/PhotoDto.kt
@@ -3,13 +3,12 @@ package com.mrkekovich.courses.dto
 import com.mrkekovich.courses.models.PhotoEntity
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
-import org.hibernate.validator.constraints.Length
+import org.springframework.web.multipart.MultipartFile
 
 /**
  * @property fileName File name of a photo.
  */
 sealed class PhotoDto : BaseDto<PhotoEntity, String>() {
-    @get:Length(min = 5, max = 100)
     @get:NotNull
     @get:NotBlank
     abstract val fileName: String?
@@ -42,10 +41,15 @@ sealed class PhotoDto : BaseDto<PhotoEntity, String>() {
     }
 
     data class Request(
-        override val fileName: String?,
-
-        @get:NotNull
-        @get:NotBlank
-        override val id: String?
+        override val fileName: String?
     ) : PhotoDto()
+
+    data class UploadRequest(
+        @NotNull
+        @NotBlank
+        val file: MultipartFile?,
+    ) : PhotoDto() {
+        override val fileName: String?
+            get() = file?.originalFilename
+    }
 }
