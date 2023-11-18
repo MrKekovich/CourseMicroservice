113e94071d8a16c7b5d3175a52dce95309d86be3
MrKekovich <mrkekovich.official@gmail.com>
2023-11-18 10:45:02 +0300

    refactor: Changed implementation of CourseService

---
+++ src/main/kotlin/com/mrkekovich/courses/controllers/CourseController.kt
@@ -0,0 +1,77 @@
+package com.mrkekovich.courses.controllers
+
+import com.mrkekovich.courses.dto.CourseDto
+import com.mrkekovich.courses.services.CourseService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.*
+
+@RequestMapping("/api/v1/courses")
+@RestController
+@Tag(name = "Course controller")
+class CourseController(
+    private val courseService: CourseService,
+) {
+    @Operation(summary = "Get all courses")
+    @ApiResponse(
+        responseCode = "200",
+        description = "Get all course with a limit",
+        content = [Content(
+            schema = Schema(implementation = CourseDto.Response.Base::class)
+        )]
+    )
+    @GetMapping("/get/all")
+    fun getCourses(
+        @Validated
+        dto: CourseDto.Request.GetAll,
+    ): ResponseEntity<List<CourseDto.Response.Base>> =
+        courseService.getCourses(dto)
+
+    @Operation(summary = "Create course")
+    @ApiResponse(
+        responseCode = "200",
+        description = "Create course",
+        content = [Content(
+            schema = Schema(implementation = CourseDto.Response.Base::class)
+        )]
+    )
+    @PostMapping("/create")
+    fun createCourse(
+        @Validated
+        dto: CourseDto.Request.Create,
+    ): ResponseEntity<CourseDto.Response.Base> =
+        courseService.createCourse(dto)
+
+    @Operation(summary = "Update course")
+    @ApiResponse(
+        responseCode = "200",
+        description = "Update course",
+        content = [Content(
+            schema = Schema(implementation = CourseDto.Response.Base::class)
+        )]
+    )
+    @PatchMapping("/update")
+    fun updateCourse(
+        @Validated
+        dto: CourseDto.Request.Update,
+    ): ResponseEntity<CourseDto.Response.Base> =
+        courseService.updateCourse(dto)
+
+    @Operation(summary = "Delete course")
+    @ApiResponse(
+        responseCode = "200",
+        description = "Delete course",
+    )
+    @DeleteMapping("/delete")
+    fun deleteCourse(
+        @Validated
+        dto: CourseDto.Request.Delete,
+    ): ResponseEntity<HttpStatus> =
+        courseService.deleteCourse(dto)
+}
--- src/main/kotlin/com/mrkekovich/courses/services/CourseService.kt
+++ src/main/kotlin/com/mrkekovich/courses/services/CourseService.kt
@@ -1,7 +1,68 @@
 package com.mrkekovich.courses.services
 
+import com.mrkekovich.courses.dto.CourseDto
+import com.mrkekovich.courses.exceptions.NotFoundException
+import com.mrkekovich.courses.models.CourseEntity
+import com.mrkekovich.courses.repositories.CourseRepository
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
+import kotlin.jvm.optionals.getOrElse
 
 @Service
-class CourseService {
+class CourseService(
+    private val courseRepository: CourseRepository,
+) {
+    private fun CourseEntity.toResponse(): CourseDto.Response.Base =
+        CourseDto.Response.Base(this)
+
+    private fun CourseDto.Request.Create.toEntity(): CourseEntity =
+        CourseEntity(
+            title = this.title,
+            description = this.description
+        )
+
+    fun getCourses(
+        dto: CourseDto.Request.GetAll,
+    ): ResponseEntity<List<CourseDto.Response.Base>> {
+        val response = courseRepository.findAll().map {
+            it.toResponse()
+        }
+
+        return ResponseEntity.ok(response)
+    }
+
+    fun createCourse(
+        dto: CourseDto.Request.Create,
+    ): ResponseEntity<CourseDto.Response.Base> {
+        val entity = courseRepository.save(
+            dto.toEntity()
+        )
+        return ResponseEntity.ok(entity.toResponse())
+    }
+
+    fun updateCourse(
+        dto: CourseDto.Request.Update,
+    ): ResponseEntity<CourseDto.Response.Base> {
+        val entity = courseRepository.findById(dto.id).getOrElse {
+            throw NotFoundException("Course ${dto.id} not found")
+        }
+        val newEntity =courseRepository.save(entity.copy(
+            title = dto.title,
+            description = dto.description
+        ))
+
+        return ResponseEntity.ok(newEntity.toResponse())
+
+    }
+
+    fun deleteCourse(
+        dto: CourseDto.Request.Delete,
+    ): ResponseEntity<HttpStatus> {
+        val entity = courseRepository.findById(dto.id).getOrElse {
+            throw NotFoundException("Course ${dto.id} not found")
+        }
+        courseRepository.delete(entity)
+        return ResponseEntity(HttpStatus.OK)
+    }
 }
