6c0a45fe462dad006382f7aae56a26fd1327ad60
MrKekovich <mrkekovich.official@gmail.com>
2023-11-23 09:23:04 +0300

    fix: Fixed request mapping to be api/v1 for ArticleController

--- src/main/kotlin/com/mrkekovich/courses/controllers/ArticleController.kt
+++ src/main/kotlin/com/mrkekovich/courses/controllers/ArticleController.kt
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/articles")
+@RequestMapping("/api/v1/articles")
 @Tag(name = "Article controller")
 class ArticleController(
     private val articleService: ArticleService
