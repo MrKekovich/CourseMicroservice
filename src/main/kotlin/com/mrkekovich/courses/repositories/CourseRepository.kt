f853f936b2a7ea225400fb2a507ca35127308635
MrKekovich <mrkekovich.official@gmail.com>
2023-11-29 09:13:21 +0300

    feat: Added filter to CourseRepository

--- src/main/kotlin/com/mrkekovich/courses/repositories/CourseRepository.kt
+++ src/main/kotlin/com/mrkekovich/courses/repositories/CourseRepository.kt
@@ -1,9 +1,38 @@
 package com.mrkekovich.courses.repositories
 
 import com.mrkekovich.courses.models.CourseEntity
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 /**
  * Course JPA repository.
  */
-interface CourseRepository : JpaRepository<CourseEntity, String>
+interface CourseRepository : JpaRepository<CourseEntity, String> {
+    @Query(
+        """
+            SELECT course FROM CourseEntity course
+            WHERE 
+                (:title IS NULL 
+                    OR lower(course.title) LIKE concat('%', lower(:title), '%'))
+                AND (:description IS NULL 
+                    OR lower(course.description) LIKE concat('%', lower(:description), '%'))
+                AND (:id IS NULL 
+                    OR course.id = :id)
+        """
+    )
+    fun findAllByFilter(
+        @Param("title")
+        title: String?,
+
+        @Param("description")
+        description: String?,
+
+        @Param("id")
+        id: String?,
+
+        pageable: Pageable,
+    ): Page<CourseEntity>
+}
